62be6d68f64bb92e49c5188616c97b039f1ad2bf
whuzxq <13026302401@163.com>
2018-10-27 14:46:34 +0800

    add camera

---
+++ threejs/geometry/webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+    mode:'development',
+    devtool: 'eval-source-map',
+    // entry: './index.js',
+    // output: {
+    //     path: path.resolve(__dirname, 'dist'),
+    //     filename: 'main.js'
+    // }
+  };
