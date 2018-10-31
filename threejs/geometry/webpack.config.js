6249bf5211bb79160bdca10426cf21764dd706cc
whuzxq <13026302401@163.com>
2018-10-31 22:57:32 +0800

    halloween

--- threejs/geometry/webpack.config.js
+++ threejs/geometry/webpack.config.js
@@ -5,9 +5,9 @@ var webpack = require('webpack');
 module.exports = {
     mode:'development',
     devtool: 'eval-source-map',
-    // entry: './index.js',
-    // output: {
-    //     path: path.resolve(__dirname, 'dist'),
-    //     filename: 'main.js'
-    // }
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    }
   };
